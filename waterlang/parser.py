03505b7b66ffe073f257223e36532dfe790ac536
cryolithozone <furymelancholy.andjoy@gmail.com>
2024-12-29 22:08:50 +0300

    parser class, initializator + peek/consume methods

--- waterlang/parser.py
+++ waterlang/parser.py
@@ -1,3 +1,28 @@
-from enum import Enum, auto
-from dataclasses import dataclass
 from typing import List
+from waterlang.lexer import Token
+from waterlang.lang_objects import *
+
+class Parser:
+    tokens: List[Token]
+    cur: int
+    ast: List[FuncDecl]
+
+    def __init__(self, tokens: List[Token]):
+        self.tokens = tokens
+        self.cur = 0
+        self.ast = []
+
+    def peek(self, offset: int) -> Token | None:
+        try:
+            return self.tokens[self.cur + offset]
+        except IndexError:
+            return None
+        
+    def consume(self) -> Token | None:
+        try:
+            self.cur += 1
+            return self.tokens[self.cur - 1]
+        except IndexError:
+            return None
+        
+    
