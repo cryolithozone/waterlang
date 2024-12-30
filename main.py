526f6e8d3754a285c9b9edef8c5ee1f571410459
cryolithozone <furymelancholy.andjoy@gmail.com>
2024-12-30 22:27:23 +0300

    main function update: accepts out file argument, translation

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ def print_ast(ast: list[waterlang.parser.FuncDecl]) -> None:
 
 def main() -> None:
     args = sys.argv
-    if len(args) < 2:
-        print("Error: no input file provided")
+    if len(args) < 3:
+        print("Error: no input and output file provided")
         return
     in_file_name = args[1]
+    out_file_name = args[2]
     with open(in_file_name, "r") as in_file:
         lexer = waterlang.lexer.Lexer(in_file, in_file_name)
         lexer.lex()
-        result = lexer.report()
-        if not result.success:
-            for tok in result.tokens:
-                print("ERROR:", tok.loc, tok.value)
-        else:
-            for tok in result.tokens:
-                print("OK:", tok)
-        parser = waterlang.parser.Parser(result.tokens)
-        parser.parse()
-        print_ast(parser.ast)
+    result = lexer.report()
+    if not result.success:
+        for tok in result.tokens:
+            print("ERROR:", tok.loc, tok.value)
+            return
+    parser = waterlang.parser.Parser(result.tokens)
+    parser.parse()
+    with open(out_file_name + ".cpp", "w") as out_file:
+        translator = waterlang.translator.Translator(parser.ast, out_file)
+        translator.translate()
 
 
 if __name__ == "__main__":
