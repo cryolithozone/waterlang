dc95757a222e5fc040bf8b8bd1530bf723bbf6c6
cryolithozone <furymelancholy.andjoy@gmail.com>
2024-12-30 22:54:30 +0300

    human-readable indentation

--- waterlang/translator.py
+++ waterlang/translator.py
@@ -6,10 +6,18 @@ class Translator:
     ast: List[FuncDecl]
     out_file: TextIO
     current_subtree: FuncDecl | Stmt | Expr
+    nesting: int
 
     def __init__(self, ast: List[FuncDecl], out_file: TextIO):
         self.ast = ast
         self.out_file = out_file
+        self.nesting = 0
+
+    def write(self, content: str) -> None:
+        self.out_file.write(content)
+
+    def indent(self) -> None:
+        self.out_file.write(" "*self.nesting*2)
 
     def translate(self) -> None:
         for decl in self.ast:
@@ -18,36 +26,49 @@ class Translator:
 
     def func_decl(self) -> None:
         assert isinstance(self.current_subtree, FuncDecl), "expected FuncDecl in func_decl()"
-        self.out_file.write(self.current_subtree.return_type.to_cpp() + " ")
-        self.out_file.write(self.current_subtree.func_name + "(")
+        self.write(self.current_subtree.return_type.to_cpp() + " ")
+        self.write(self.current_subtree.func_name + "(")
         for arg in self.current_subtree.arg_list:
             # No arguments support yet...
             raise NotImplementedError("argument compilation is not supported")
-        self.out_file.write(")\n")
+        self.write(")\n")
         if self.current_subtree.stmt.tag is StmtType.BlockStmt:
             self.current_subtree = self.current_subtree.stmt
             self.stmt()
         else:
-            self.out_file.write("{\n")
+            self.write("{\n")
+            self.nesting += 1
             self.current_subtree = self.current_subtree.stmt
+            self.indent()
             self.stmt()
-            self.out_file.write("\n}")
-        self.out_file.write("\n\n")
+            self.write("\n")
+            self.nesting -= 1
+            self.indent()
+            self.write("}")
+        self.write("\n\n")
 
     def stmt(self) -> None:
         assert isinstance(self.current_subtree, Stmt), "expected Stmt in stmt()"
         match self.current_subtree.tag:
             case StmtType.ReturnStmt:
-                self.out_file.write("return ")
+                self.write("return ")
                 self.current_subtree = self.current_subtree.expr
                 self.expr()
-                self.out_file.write(";")
+                self.write(";")
             case StmtType.BlockStmt:
-                self.out_file.write("{\n")
-                for stmt in self.current_subtree.stmts:
+                self.write("{\n")
+                self.nesting += 1
+                len_block = len(self.current_subtree.stmts)
+                for i, stmt in enumerate(self.current_subtree.stmts):
                     self.current_subtree = stmt
+                    self.indent()
                     self.stmt()
-                self.out_file.write("\n}\n")
+                    if i != len_block - 1:
+                        self.write("\n")
+                self.write("\n")
+                self.nesting -= 1
+                self.indent()
+                self.write("}")
             case _:
                 raise NotImplementedError(f"compiling statements of type {self.current_subtree.tag} is not supported")
     
@@ -55,15 +76,15 @@ class Translator:
         assert isinstance(self.current_subtree, Expr), "expected Expr in expr()"
         match self.current_subtree.tag:
             case ExprType.Literal:
-                self.out_file.write(str(self.current_subtree.value))
+                self.write(str(self.current_subtree.value))
             case ExprType.Grouping:
-                self.out_file.write("(")
+                self.write("(")
                 self.current_subtree = self.current_subtree.expr
                 self.expr()
-                self.out_file.write(")")
+                self.write(")")
             case ExprType.Unary:
                 if self.current_subtree.negated:
-                    self.out_file.write("-")
+                    self.write("-")
                 self.current_subtree = self.current_subtree.expr
                 self.expr()
             case ExprType.Binary:
@@ -73,6 +94,6 @@ class Translator:
                 self.current_subtree = left
                 self.expr()
                 assert op.ttype == TType.OP, "expected operation in expr(), case ExprType.Binary"
-                self.out_file.write(op.value.to_cpp())
+                self.write(op.value.to_cpp())
                 self.current_subtree = right
                 self.expr()
