b8dfaf78f9fa4d6f2c8e99541942f8b37c306c36
cryolithozone <furymelancholy.andjoy@gmail.com>
2024-12-30 23:03:40 +0300

    cleaning up imports

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import waterlang
+from waterlang.parser import FuncDecl
+from waterlang.lexer import Lexer
+from waterlang.parser import Parser
+from waterlang.translator import Translator
 import sys
 import subprocess as sp
 
-def print_ast(ast: list[waterlang.parser.FuncDecl]) -> None:
+def print_ast(ast: list[FuncDecl]) -> None:
     for fdecl in ast:
         print("-------")
         print(fdecl)
@@ -15,17 +18,17 @@ def main() -> None:
     in_file_name = args[1]
     out_file_name = args[2]
     with open(in_file_name, "r") as in_file:
-        lexer = waterlang.lexer.Lexer(in_file, in_file_name)
+        lexer = Lexer(in_file, in_file_name)
         lexer.lex()
     result = lexer.report()
     if not result.success:
         for tok in result.tokens:
             print("ERROR:", tok.loc, tok.value)
             return
-    parser = waterlang.parser.Parser(result.tokens)
+    parser = Parser(result.tokens)
     parser.parse()
     with open(out_file_name + ".cpp", "w") as out_file:
-        translator = waterlang.translator.Translator(parser.ast, out_file)
+        translator = Translator(parser.ast, out_file)
         translator.translate()
     sp.run(["g++", out_file_name + ".cpp", "-o", out_file_name, "-g"])
 
