76904b2b09ceaa89d3f25e75716aa419c7a3b719
cryolithozone <furymelancholy.andjoy@gmail.com>
2025-01-03 03:38:44 +0300

    adding the waterlang header file to the translated c++

--- waterlang/translator.py
+++ waterlang/translator.py
@@ -20,6 +20,7 @@ class Translator:
         self.out_file.write(" "*self.nesting*2)
 
     def translate(self) -> None:
+        self.write("#include \"lib/waterlang.hpp\"")
         for decl in self.ast:
             self.current_subtree = decl
             self.func_decl()
